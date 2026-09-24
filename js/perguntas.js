a2c1d0df70bd58ded1532bbd1e047d0001273f4e
Emanuele05Lima <krassowski.emanuele@escola.pr.gov.br>
2026-09-24 09:55:19 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -4,13 +4,13 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Usar a IA para analisar fotos das plantas e identificar possíveis pragas ou doenças.",
-                afirmacao: "Porque a IA pode analisar imagens das plantas e reconhecer sinais de pragas ou doenças, ajudando o produtor a agir rapidamente.",
-                "A IA analisaa imagem das plantas e roconhece pragas e doenças, auxiliando o pordutor"
+                afirmacao: ["Porque a IA pode analisar imagens das plantas e reconhecer sinais de pragas ou doenças, ajudando o produtor a agir rapidamente.",
+                "A IA analisaa imagem das plantas e roconhece pragas e doenças, auxiliando o pordutor"]
             },
             {
                 texto:  "Usar a IA para analisar dados da lavoura e indicar quais áreas precisam de maior atenção. ",
-                afirmacao: "Porque a IA pode identificar áreas mais afetadas, permitindo um tratamento mais preciso e evitando o uso desnecessário de agrotóxicos. Isso pode diminuir a contaminação e ajudar na preservação da qualidade do solo.", 
-                "A IA identifica áreas afetadas, permetindo um tratamento sem agredir o meio ambiente"
+                afirmacao: ["Porque a IA pode identificar áreas mais afetadas, permitindo um tratamento mais preciso e evitando o uso desnecessário de agrotóxicos. Isso pode diminuir a contaminação e ajudar na preservação da qualidade do solo.", 
+                "A IA identifica áreas afetadas, permetindo um tratamento sem agredir o meio ambiente"]
             }    
            
         ]
@@ -21,13 +21,13 @@ export const perguntas = [
             alternativas: [
                 {
                     texto: "Permitindo a aplicação localizada de defensivos biológicos ou químicos apenas nas áreas afetadas (pulverização de precisão), evitando o acúmulo desnecessário de resíduos tóxicos em todo o terreno.",
-                    afirmacao: "A IA aliada a sensores e drones permite o mapeamento exato de onde as pragas estão concentradas. Em vez de aplicar veneno em toda a plantação (aplicação em área total), o produtor realiza a pulverização cirúrgica. ",
-                    " A IA visualisa onde estão mais concentadas as pragas, aplicando somente ali o produdo em precisar andar a lavoura inteira"
+                    afirmacao: ["A IA aliada a sensores e drones permite o mapeamento exato de onde as pragas estão concentradas. Em vez de aplicar veneno em toda a plantação (aplicação em área total), o produtor realiza a pulverização cirúrgica. ",
+                    " A IA visualisa onde estão mais concentadas as pragas, aplicando somente ali o produdo em precisar andar a lavoura inteira"]
                 },
                 {
                     texto:  "Viabilizando o uso de armadilhas inteligentes e dados preditivos que ajudam o produtor a antecipar infestações, reduzindo o volume geral de agroquímicos despejados no solo. ",
-                    afirmacao: "Ao cruzar dados meteorológicos com imagens de armadilhas automáticas, a IA prevê quando uma infestação vai começar. Isso possibilita ações preventivas mais limpas, diminuindo a dependência de defensivos.",
-                    "Ao correlacionar dados do clima com capturas de armadilhas automatizadas, a inteligência artificial antecipa o início de infestações, permitindo o controle preventivo e reduzindo o uso de agroquímicos."
+                    afirmacao: ["Ao cruzar dados meteorológicos com imagens de armadilhas automáticas, a IA prevê quando uma infestação vai começar. Isso possibilita ações preventivas mais limpas, diminuindo a dependência de defensivos.",
+                    "Ao correlacionar dados do clima com capturas de armadilhas automatizadas, a inteligência artificial antecipa o início de infestações, permitindo o controle preventivo e reduzindo o uso de agroquímicos."]
                 }    
                
             ]
@@ -38,14 +38,15 @@ export const perguntas = [
             alternativas: [
                 {
                     texto: "A IA evita diagnósticos errados (como aplicar fungicida para resolver um problema de inseto), o que impede a sobrecarga da terra com substâncias químicas inúteis.",
-                    afirmacao: "A identificação humana visual e demorada muitas vezes gera erros de diagnóstico. Tratar uma praga com o produto errado gera um custo financeiro alto e um impacto ambiental grave, poluindo o solo com um elemento tóxico desnecessário.",
-                    "Quando um problema agrícola é tratado com o defensivo errado, as consequências são graves: há um alto prejuízo financeiro com produtos ineficazes e um sério impacto ambiental, causado pela contaminação desnecessária do solo com substâncias tóxicas."
+                    afirmacao: ["A identificação humana visual e demorada muitas vezes gera erros de diagnóstico. Tratar uma praga com o produto errado gera um custo financeiro alto e um impacto ambiental grave, poluindo o solo com um elemento tóxico desnecessário.",
+                    "Quando um problema agrícola é tratado com o defensivo errado, as consequências são graves: há um alto prejuízo financeiro com produtos ineficazes e um sério impacto ambiental, causado pela contaminação desnecessária do solo com substâncias tóxicas." ]
                 },
                 {
                     texto:    "Algoritmos inteligentes integrados ao maquinário ajudam a diminuir a lixivação e o escoamento superficial de poluentes ao cruzar dados de infestação com a umidade ideal do solo.",
                
-                    afirmacao: "Sistemas inteligentes sabem correlacionar o manejo com as condições climáticas e a textura do solo. Aplicar o tratamento correto no momento em que o solo não sofrerá com fortes chuvas, por exemplo, evita que o defensivo infiltre incorretamente.",
-                    "Aplicar o defensivo no momento ideal — evitando períodos de chuvas fortes, por exemplo — previne a infiltração incorreta do produto e protege o solo."
+                    afirmacao: ["Sistemas inteligentes sabem correlacionar o manejo com as condições climáticas e a textura do solo. Aplicar o tratamento correto no momento em que o solo não sofrerá com fortes chuvas, por exemplo, evita que o defensivo infiltre incorretamente.",
+                    "Aplicar o defensivo no momento ideal — evitando períodos de chuvas fortes, por exemplo — previne a infiltração incorreta do produto e protege o solo." ]
+
                 }    
                
             ]
